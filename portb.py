c2af79b27b8d22877897ccc7e09b5aaea5e93e62
Pratik katti <90851204+pratt4@users.noreply.github.com>
2023-05-20 08:10:38 +0530

    Add files via upload

---
+++ portb.py
@@ -0,0 +1,71 @@
+import socket
+import time
+import threading
+from scapy.all import ARP, Ether, srp
+from queue import Queue
+
+
+socket.setdefaulttimeout(0.25)
+print_lock = threading.Lock()
+
+target = input('Enter the host to be scanned: ')
+t_IP = socket.gethostbyname(target)
+print ('Starting scan on host: ', t_IP)
+
+def portscan(port):
+   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   try:
+      con = s.connect((t_IP, port))
+      with print_lock:
+         print(port, 'is open')
+      con.close()
+   except:
+      pass
+
+def threader():
+   while True:
+      worker = q.get()
+      portscan(worker)
+      q.task_done()
+      
+q = Queue()
+startTime = time.time()
+   
+for x in range(100):
+   t = threading.Thread(target = threader)
+   t.daemon = True
+   t.start()
+   
+for worker in range(1, 1000):
+   q.put(worker)
+   
+q.join()
+print('Time taken:', time.time() - startTime)
+
+
+
+####################################
+target_ip = "192.168.184.76/24"
+# IP Address for the destination
+# create ARP packet
+arp = ARP(pdst=target_ip)
+# create the Ether broadcast packet
+# ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+# stack them
+packet = ether/arp
+
+result = srp(packet, timeout=3, verbose=0)[0]
+
+# a list of clients, we will fill this in the upcoming loop
+clients = []
+
+for sent, received in result:
+    # for each response, append ip and mac address to `clients` list
+    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
+# print clients
+print("Available devices in the network:")
+print("IP" + " "*18+"MAC")
+for client in clients:
+    print("{:16}    {}".format(client['ip'], client['mac']))
